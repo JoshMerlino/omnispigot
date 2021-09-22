1fdb85027a0db33e59299c50adb717ec0ed13f9f
Josh Merlino <joshua.merlino@gmail.com>
2021-09-21 21:08:01 -0400

    remove async

--- src/util/inject.ts
+++ src/util/inject.ts
@@ -7,7 +7,7 @@ const { webserver } = JSON.parse(readFileSync(resolve("./package.json"), "utf8")
 
 export default function inject(url: string, req: Request, target: string | null = null): Promise<string | Record<string, unknown>> {
 	process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-	return new Promise(async resolve => {
+	return new Promise(resolve => {
 		if (!req.query.hasOwnProperty("deep") && req.header("deep") !== "true") return resolve(url);
 		fetch(`http://localhost:${process.env.PORT || webserver.http.port}${url}`)
 			.then(response => response.json())
